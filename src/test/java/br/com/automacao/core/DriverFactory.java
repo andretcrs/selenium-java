de95671a05608e665c8a73ce7cdbaca14c764c15
André Scheffer <andre.scheffer@dbserver.com.br>
2026-01-26 13:24:30 -0300

    docs: adiciona workflow do github actions e config headless

--- src/test/java/br/com/automacao/core/DriverFactory.java
+++ src/test/java/br/com/automacao/core/DriverFactory.java
@@ -4,6 +4,7 @@ import br.com.automacao.config.ConfigManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -11,15 +12,28 @@ public class DriverFactory {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-
             String browser = ConfigManager.get("browser");
 
             if (browser.equalsIgnoreCase("chrome")) {
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+
+                ChromeOptions options = new ChromeOptions();
+
+                boolean isCI = System.getenv("GITHUB_ACTIONS") != null;
+
+                if (isCI) {
+                    options.addArguments("--headless=new"); // Roda sem abrir janela no GitHub
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    options.addArguments("--window-size=1920,1080");
+                }
+
+                driver = new ChromeDriver(options);
             }
 
-            driver.manage().window().maximize();
+            if (driver != null) {
+                driver.manage().window().maximize();
+            }
         }
         return driver;
     }
